e606327da7456b4931466062d393dbe6101b843d
Gautam Ramesh <gautamshivramesh@gmail.com>
2016-11-20 12:35:57 -0500

    Worked on Master Features file

---
+++ Neural_Network/master_features.py
@@ -0,0 +1,18 @@
+import sys
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+COLOR_THRESH = 150
+EDGE_MIN_THRESH = 100
+EDGE_MAX_THRESH = 200
+
+def features(img):
+    color = cv2.cvtColor(detect(img, COLOR_THRESH), cv2.COLOR_BGR2GRAY).flatten()
+    edge = cv2.cvtColor(cv2.Canny(img,EDGE_MIN_THRESH,EDGE_MAX_THRESH), cv2.COLOR_BGR2GRAY).flatten()
+    grayscale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY).flatten()
+
+    return np.append(color, edge, grayscale)
+
+def crop(img, coords)
+    
